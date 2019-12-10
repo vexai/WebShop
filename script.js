9bc20ab2d0184301be0319c05654fb5c9b762777
vexai <dominik.przystal@gmail.com>
2019-12-10 23:04:23 +0100

    video

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    $('#button-goto1').hover(function() {
+        $('#img1').css('box-shadow','wheat 5px 5px' );
+    }, function() {
+        $('#img1').css('box-shadow', '')
+    });
+
+    $('#button-goto2').hover(function() {
+        $('#img2').css('box-shadow','wheat 5px 5px' );
+    }, function() {
+        $('#img2').css('box-shadow', '')
+    });
+
+    $('#button-goto3').hover(function() {
+        $('#img3').css('box-shadow','wheat 5px 5px' );
+    }, function() {
+        $('#img3').css('box-shadow', '')
+    });
+
+});
