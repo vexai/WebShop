ddb7aeffb4742d7a3b78bc1043ea0d5ce1d9270f
vexai <dominik.przystal@gmail.com>
2019-12-11 01:46:04 +0100

    delivery parallax

---
+++ Delivery/script_delivery.js
@@ -0,0 +1,12 @@
+
+$(function () {
+$('.delivery-card').hover(function () {
+    $('.h5').css('color','darkred');
+    $('.h5big').css('font-size', '40px');
+},function () {
+    $('.h5').css('color','');
+    $('.h5big').css('font-size', '50px');
+})
+
+
+});
